65843e5dd3f082fcdaae09e25eee5c6d7a51e3e6
lea <relojalthea@gmail.com>
2025-10-23 15:19:37 +0800

    Update App.jsx

--- creo-app-hub/central-site/src/App.jsx
+++ creo-app-hub/central-site/src/App.jsx
@@ -176,7 +176,7 @@ function App() {
   const pageStyle = {
     minHeight: '100vh',
     width: '100%',
-    background: '#transparent',
+    background: '#e4be85ff',
     display: 'flex',
     flexDirection: 'column',
   }
